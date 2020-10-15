03eb63ff440c3071a9bc26d9cfe0ceb9daa5998c
staglizd <stipe.staglicic@gmail.com>
2020-10-15 12:52:12 +0200

    Saving run into database

--- app/src/main/java/com/example/nextrun/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/nextrun/ui/viewmodels/MainViewModel.kt
@@ -2,9 +2,16 @@ package com.example.nextrun.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.nextrun.db.Run
 import com.example.nextrun.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
